1999322c36ed05779ee47e2156b83b8fa856be36
Abdulla08 <abdullazakipov8@gmail.com>
2022-01-04 12:07:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homeworke5/PersonViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.homeworke5;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PersonViewHolder extends RecyclerView.ViewHolder {
+    private TextView name;
+
+    public PersonViewHolder(@NonNull View itemView) {
+        super(itemView);
+        name = itemView.findViewById(R.id.name);
+    }
+
+    public void onBind(String name){
+        this.name.setText(name);
+    }
+}
